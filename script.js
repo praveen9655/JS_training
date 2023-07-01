f88a932f12b431d1a4c726413d1e89482c5539c3
praveen9655 <praveen965592@email.com>
2023-07-01 06:47:11 +0530

    updated

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function myFuc(){
     document.getElementById('styC').style.color='blue'
 }
+function myFuc1(){
+    let x,y,z;
+    x=10;
+    y=10;
+    z=x+y;
+    document.getElementById('demo1').innerHTML ='The z value is :'+z;
+}
